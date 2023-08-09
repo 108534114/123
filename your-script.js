0b8b2836d9493bf5d2cf7be3f18f025a064cca77
108534157 <108534157@stu.ukn.edu.tw>
2023-08-09 15:11:31 +0800

    2

--- your-script.js
+++ your-script.js
@@ -19,3 +19,32 @@ document.addEventListener('DOMContentLoaded', function () {
     const saveButton = document.getElementById('save-button');
     saveButton.addEventListener('click', saveTableToDatabase);
 });
+// 在 randomFillTable 函数中的循环中添加以下代码
+cell.textContent = `學號: ${selectedStudent}`;
+cell.style.textDecoration = 'none'; // 默认情况下移除横线样式
+
+// 添加点击事件，用于切换选中状态时改变横线样式
+cell.addEventListener('click', function () {
+    if (cell.style.textDecoration === 'line-through') {
+        cell.style.textDecoration = 'none';
+    } else {
+        cell.style.textDecoration = 'line-through';
+    }
+});
+const selectedStudents = Array.from(document.querySelectorAll('input[name="studentList"]:checked'))
+    .map(checkbox => checkbox.value);
+
+// ...
+
+if (selectedStudents.length === 0) {
+    console.log("請選擇至少一位學生。");
+    return;
+}
+
+// ...
+
+// 在生成 cell 时，根据已选择的学生为其添加横线样式
+if (selectedStudents.includes(selectedStudent)) {
+    cell.style.textDecoration = 'line-through';
+}
+
