2b644a82108208ea861d960d0866c3ce71c89fb9
108534157 <108534157@stu.ukn.edu.tw>
2023-08-09 15:41:49 +0800

    2

--- your-script.js
+++ your-script.js
@@ -1,35 +1,20 @@
-// 选中所有学生的函数
+// 更新 "全部選取" 按钮的状态
+function updateSelectAllButton() {
+    const studentCheckboxes = document.querySelectorAll('input[name="studentList"]');
+    const selectAllButton = document.getElementById('btn_all');
+
+    // 检查是否所有复选框都被选中
+    const allSelected = Array.from(studentCheckboxes).every(checkbox => checkbox.checked);
+
+    // 更新 "全部選取" 按钮的选中状态
+    selectAllButton.checked = allSelected;
+}
+
+// 在 "全部選取" 按钮的点击事件中调用 updateSelectAllButton
 function selectAllStudents() {
     const checkboxes = document.querySelectorAll('input[name="studentList"]');
     checkboxes.forEach(checkbox => {
         checkbox.checked = true;
     });
+    updateSelectAllButton(); // 更新按钮状态
 }
-
-// 将表格数据保存到数据库的函数
-function saveTableToDatabase() {
-    const xhr = new XMLHttpRequest();
-    xhr.open('POST', '座位資料表.php', true);
-    xhr.setRequestHeader('Content-Type', 'application/json;charset=UTF-8');
-    xhr.onreadystatechange = function () {
-        if (xhr.readyState === 4) {
-            if (xhr.status === 200) {
-                console.log(xhr.responseText);
-            } else {
-                console.error('AJAX request failed:', xhr.status, xhr.statusText);
-            }
-        }
-    };
-    xhr.send(JSON.stringify({ tableData: tableData }));
-}
-
-// 在 DOMContentLoaded 事件中绑定按钮点击事件
-document.addEventListener('DOMContentLoaded', function () {
-    // ... 其他代码 ...
-
-    const selectAllButton = document.getElementById('btn_all');
-    selectAllButton.addEventListener('click', selectAllStudents);
-
-    const saveButton = document.getElementById('save-button');
-    saveButton.addEventListener('click', saveTableToDatabase);
-});
