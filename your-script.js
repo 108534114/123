251126ab46b2c933dc80b7097219acbc7e532166
108534157 <108534157@stu.ukn.edu.tw>
2023-08-09 15:29:16 +0800

    2

--- your-script.js
+++ your-script.js
@@ -1,25 +1,26 @@
 function saveTableToDatabase() {
-    // 將表格資料作為 JavaScript 物件傳送到後端 PHP 檔案
-    $.ajax({
-        type: 'POST',
-        url: '座位資料表.php', // 請將 save-to-database.php 替換為你的後端 PHP 檔案路徑
-        data: { tableData: JSON.stringify(tableData) },
-        success: function (response) {
-            console.log(response); // PHP 檔案回傳的訊息會顯示在瀏覽器的開發者工具中
-        },
-        error: function (xhr, status, error) {
-            console.error(error); // 顯示錯誤訊息
-        },
-    });
+    const xhr = new XMLHttpRequest();
+    xhr.open('POST', '座位資料表.php', true);
+    xhr.setRequestHeader('Content-Type', 'application/json;charset=UTF-8');
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4) {
+            if (xhr.status === 200) {
+                console.log(xhr.responseText);
+            } else {
+                console.error('AJAX request failed:', xhr.status, xhr.statusText);
+            }
+        }
+    };
+    xhr.send(JSON.stringify({ tableData: tableData }));
 }
+
+// 在 DOMContentLoaded 事件中绑定按钮点击事件
 document.addEventListener('DOMContentLoaded', function () {
-    // ... 其他程式碼 ...
+    // ... 其他代码 ...
 
-  // 綁定 "全部選取" 按鈕的點擊事件
-  const selectAllButton = document.getElementById('btn_all');
-  selectAllButton.addEventListener('click', selectAllStudents);
+    const selectAllButton = document.getElementById('btn_all');
+    selectAllButton.addEventListener('click', selectAllStudents);
 
-    // 綁定 "儲存" 按鈕的點擊事件
     const saveButton = document.getElementById('save-button');
     saveButton.addEventListener('click', saveTableToDatabase);
 });
